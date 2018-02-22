9401bb5e9c83d77afb0d380a6363d847b47d9646
oren.burstein <oren.burstein@gmail.com>
2018-02-22 20:28:23 +0200

    Init

---
+++ Basic Concepts/src/MyClass.java
@@ -0,0 +1,18 @@
+
+/*************************************
+ * This class is for learning only
+ * name of the class is MyClass 
+ */
+public class MyClass {
+    public static void main(String[ ] args) {
+    	// Print a string on the console.
+        System.out.println("Hello World"); 
+        
+        
+        /*************
+          This is the second line printed
+         *************/
+        System.out.println("Hello World again");
+        
+    }
+}

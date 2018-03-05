8c9b030684bc142ca5adeb611685c3186ceb5762
oren.burstein <oren.burstein@gmail.com>
2018-03-05 18:07:19 +0200

    while..break

--- Conditionals and Loops/src/MyClass2.java
+++ Conditionals and Loops/src/MyClass2.java
@@ -1,11 +1,18 @@
 
 public class MyClass2 {
     public static void main(String[ ] args) {
+
+
     	int x = 1;
-    	do {
-    	  System.out.println(x);
+
+    	while(x > 0) {
+    	 System.out.println(x);
+    	  if(x == 4) {
+    	    break;
+    	  }
     	  x++;
-    	} while(x < 0);
+    	}
+    	
     		
     }
 }

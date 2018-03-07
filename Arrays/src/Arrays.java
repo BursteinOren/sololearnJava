90e36d0c26fb171cd2fb926a491af142573d011a
orenb <orenb@OshritPC>
2018-03-07 21:23:53 +0200

    Arrays sample

---
+++ Arrays/src/Arrays.java
@@ -0,0 +1,19 @@
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+		System.out.println("________________");
+		
+		//int[] arr;
+		
+		int[] arr = new int[5];
+		arr[2]=42;
+		for (int i=0;i<5;i++) {
+			System.out.println("The value of cell "+i+" is "+arr[i]);
+		}
+		
+	}
+
+}

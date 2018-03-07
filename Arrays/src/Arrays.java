9f932cd4970c194935c05b24606cbbe00e3b9dd5
orenb <orenb@OshritPC>
2018-03-07 21:42:32 +0200

    arrays with comments

--- Arrays/src/Arrays.java
+++ Arrays/src/Arrays.java
@@ -1,13 +1,21 @@
-
+/**
+ * class description for testing javadocs purpose
+ * @author orenb
+ *
+ */
 public class Arrays {
-
+/**
+ * main method description.
+ * @param args
+ */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi");
 		System.out.println("________________");
 		
-		//int[] arr;
-		
+		/**
+		 * create array sample
+		 */
 		int[] arr = new int[5];
 		arr[2]=42;
 		for (int i=0;i<5;i++) {

cd4b6f7d66a7ebbf6cc7343217490436da7722f7
orenb <orenb@OshritPC>
2018-03-10 08:55:59 +0200

    Array length

--- Arrays/src/Arrays.java
+++ Arrays/src/Arrays.java
@@ -1,24 +1,28 @@
 /**
  * class description for testing javadocs purpose
+ * 
  * @author orenb
  *
  */
 public class Arrays {
-/**
- * main method description.
- * @param args
- */
+	/**
+	 * main method description.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi");
 		System.out.println("________________");
-		
+
 		/**
 		 * create array sample
 		 */
-		String[] myNames = { "A", "B", "C", "D"};
+		String[] myNames = { "A", "B", "C", "D" };
 		System.out.println(myNames[2]);
 		
+		int[ ] intArr = new int[5];
+		System.out.println(intArr.length);
 	}
 
 }

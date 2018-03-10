270430feb6a0d3af8e46ec40a897609a240bfe74
orenb <orenb@OshritPC>
2018-03-10 17:27:15 +0200

    sayHello

---
+++ Classes and Objects/src/myClassChapter3.java
@@ -0,0 +1,13 @@
+
+public class myClassChapter3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   sayHello();
+		
+		
+	}
+	static void sayHello() {
+		    System.out.println("Hello World!");
+		  }
+}

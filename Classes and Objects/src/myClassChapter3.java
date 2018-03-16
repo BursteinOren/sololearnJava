470fffd1063681a3669ed72be579612ae93c6aea
orenb <orenb@DESKTOP-O>
2018-03-16 22:03:43 +0200

    Constractor

--- Classes and Objects/src/myClassChapter3.java
+++ Classes and Objects/src/myClassChapter3.java
@@ -3,13 +3,16 @@ public class myClassChapter3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		   sayHello();
-		   sayHello();
+		   sayHello("David");
+		   sayHello("Amy");
 		   sayHello();
 		
 		
 	}
 	static void sayHello() {
-		    System.out.println("Hello World!");
-		  }
+		System.out.println("Hello world!");
+	}
+	static void sayHello(String name) {
+		System.out.println("Hello "+name);
+	}
 }

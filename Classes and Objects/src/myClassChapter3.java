18df49a2e7eeee0511dcfc79cec6e6e3767a80a7
orenb <orenb@DESKTOP-O>
2018-03-17 17:09:25 +0200

    Value Types

--- Classes and Objects/src/myClassChapter3.java
+++ Classes and Objects/src/myClassChapter3.java
@@ -1,29 +1,13 @@
 
 public class myClassChapter3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int x = sum(2,5);
-		System.out.println(x);
-		
-		
-		System.out.println("----------------------");
-		Animal dog = new Animal();
-		dog.bark();
-		
-		System.out.println("----------------------");
-		Vehicle v1 = new Vehicle();
-		Vehicle v2 = new Vehicle("Green");
-		//v.setColor("Blue");
-		v1.horn();
-		System.out.println(v1.getColor());
-		System.out.println(v2.getColor());
-		
-		
-		
-	}
-
-	static int sum(int val1,int val2) {
-		return val1 +val2;
-	}
+	  public static void main(String[ ] args) {
+	    int x = 5;
+	    addOneTo(x);
+	    System.out.println(x);       
+	  }
+	  static void addOneTo(int num) {
+	    num =+ 1;
+	  }
+	
 }

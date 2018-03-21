e765a09e72980a418bc9d2c64b58743e17cf76cb
Oren Burstein <oren.burstein@gmail.com>
2018-03-21 11:01:15 +0200

    Anonymous Classes

--- More on Classes/src/MyClass.java
+++ More on Classes/src/MyClass.java
@@ -1,12 +1,20 @@
-
+class Machine {
+  public void start() {
+    System.out.println("Starting...");
+  }
+}  
 public class MyClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	Dog d = new Dog();
-    d.eat();
-    d.makeSound();
+
     
+	Machine m1 = new Machine() {
+	@Override public void start() {
+	  System.out.println("Wooooo");
+	    }
+	  };
+      Machine m2 = new Machine();
+      m2.start();
 	}
 
 	

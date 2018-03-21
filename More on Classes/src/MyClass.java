6b4949a3298264049bbdd5a8989e4f18de9773e4
Oren Burstein <oren.burstein@gmail.com>
2018-03-21 12:08:27 +0200

    The equals() method

--- More on Classes/src/MyClass.java
+++ More on Classes/src/MyClass.java
@@ -2,7 +2,13 @@ class MyClass {
 	public static void main(String[ ] args) {
     Animal a1 = new Animal("Robby");
     Animal a2 = new Animal("Robby");
+    System.out.println(a1.hashCode());
+    System.out.println(a1.name.hashCode());
+    System.out.println(a1.getClass());
+    System.out.println("----------------");
     System.out.println(a1 == a2);
+    System.out.println("----------------");
+    System.out.println(a1.equals(a2));
 	}
 }
 //Outputs false

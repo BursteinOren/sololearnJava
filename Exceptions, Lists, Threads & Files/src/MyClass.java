0621bd50ffc6fe1aa0008f121b7120ad444d8cac
Oren Burstein <oren.burstein@gmail.com>
2018-03-22 14:35:27 +0200

    throw Exception

--- Exceptions, Lists, Threads & Files/src/MyClass.java
+++ Exceptions, Lists, Threads & Files/src/MyClass.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 
 public class MyClass {
 
@@ -5,16 +6,24 @@ public class MyClass {
 		// TODO Auto-generated method stub
 		try {
 		      int a[ ] = new int[2];
-		      System.out.println(a[5]);
+		      System.out.println(a[1]);
 		    } catch (Exception e) {
 		      System.out.println("An error occurred");
 		      System.out.println("Message: "+e.getMessage());
 		      System.out.println("Cause: "+e.getCause().toString());
 		//      System.out.println("Message: "+e.getMessage());
 
-		    
 		    }
+		System.out.println("div:" + div(4,3));
+		//System.out.println("div:" + div(4,0));
 		
 	}
-
+	public static int div(int a, int b) throws ArithmeticException,IOException{
+		  if(b == 0) {
+		    throw new ArithmeticException("Division by Zero");
+		  } else {
+		    return a / b;
+		  }
+		  
+		}
 }
